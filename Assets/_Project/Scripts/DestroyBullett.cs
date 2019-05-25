ecff6b8eed281211ba8b381f283647e6fbe742d5
joel <jrattur@gmail.com>
2019-05-25 19:40:49 +1000

    Added time to life for Bullets

--- Assets/_Project/Scripts/DestroyBullett.cs
+++ Assets/_Project/Scripts/DestroyBullett.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class DestroyBullett : MonoBehaviour
 {
+    private float birthTime;
+    [SerializeField] private float lifeTime = 5f;
+
+    private void Start()
+    {
+        birthTime = Time.time;
+    }
+
+    public void Update()
+    {
+        if (Time.time > birthTime + lifeTime) { Destroy(gameObject); }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
