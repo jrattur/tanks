d9293cc007fec637f04cf6ad2b81b20f1da021bf
joel <jrattur@gmail.com>
2019-05-25 19:54:16 +1000

    Added barrel cam

---
+++ Assets/_Project/Scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject camera1, camera2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2")) {
+            camera1.SetActive(!camera1.activeInHierarchy);
+            camera2.SetActive(!camera2.activeInHierarchy);
+        }
+    }
+}
